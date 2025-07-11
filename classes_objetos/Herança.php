a08e8ca282476dc73e6ec160d1efb8068954f9ce
GPetrolini <guspetrolini@hotmail.com>
2025-07-11 17:02:36 -0300

    Novos códigos

---
+++ classes_objetos/Herança.php
@@ -0,0 +1,50 @@
+<div class="titulo">Herança</div>
+
+<?php
+
+class Pessoa {
+  public $nome;
+  public $idade;
+
+  function __construct($nome, $idade)
+  {
+    $this-> $nome;
+    $this-> $idade;
+    echo 'Pessoa criada! <br>';
+  }
+
+  function __destruct()
+  {
+    echo 'Tchau!';
+  }
+
+  public function apresentar () {
+    echo "{$this->nome}. {$this->idade} anos <br>";
+  }
+}
+
+class Usuario extends Pessoa {
+  public $login;
+
+  function __construct($nome,$idade,$login)
+  {
+    $this->nome = $nome;
+    $this->idade = $idade;
+    $this->login = $login;
+    echo 'Usuario criado! <br>';
+  }
+  function __destruct()
+  {
+    echo 'Usuário diz: tchau!';
+  }
+
+  public function apresentar()
+  {
+    echo "@{$this->login}:";
+    parent::apresentar()
+  }
+
+}
+
+$usuario = new Usuario ('Gustavo Mendonça', 21, 'gust_mend');
+$usuario->apresentar();
